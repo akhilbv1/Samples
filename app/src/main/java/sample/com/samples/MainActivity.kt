74ea4aeb6adfab1c5a1805cf020cf379d546ba34
akhilbv1 <akhilbv1@gmail.com>
2019-04-03 13:43:36 +0530

    Initial commit

---
+++ app/src/main/java/sample/com/samples/MainActivity.kt
@@ -0,0 +1,13 @@
+package sample.com.samples
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sample_login_ll)
+    }
+}
